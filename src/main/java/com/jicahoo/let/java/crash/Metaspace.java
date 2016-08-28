745b5e437b17bb43b08b3e6bcc09a31a4bae9c67
Jichao <jicahoo@163.com>
2016-08-28 04:24:20 -0400

    Add function of loadding class from file
    The file contains the class name line by line

--- src/main/java/com/jicahoo/let/java/crash/Metaspace.java
+++ src/main/java/com/jicahoo/let/java/crash/Metaspace.java
@@ -1,10 +1,17 @@
 package com.jicahoo.let.java.crash;
 
+import com.jichao.letjavacrash.util.ClassLoadUtil;
+
 public class Metaspace {
     static javassist.ClassPool cp = javassist.ClassPool.getDefault();
 
     public static void main(String[] args) throws Exception{
-        for (long i = 0; ; i++) { 
+        System.out.println(args[0]);
+        ClassLoadUtil.loadFromFile(args[0]);
+    }
+
+    public static void useJavassist() throws Exception {
+        for (long i = 0; ; i++) {
             Class c = cp.makeClass("eu.plumbr.demo.Generated" + i).toClass();
             System.out.println(i);
             Thread.sleep(10*1000);
---
+++ src/main/java/com/jichao/letjavacrash/util/ClassLoadUtil.java
@@ -0,0 +1,32 @@
+package com.jichao.letjavacrash.util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Created by jichao on 2016/8/28.
+ */
+public class ClassLoadUtil {
+
+    public static List<Class> loadFromFile(final String filePath) {
+        List<Class> list = new LinkedList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                Class clazz = Class.forName(line);
+                list.add(clazz);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
